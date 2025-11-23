6efb4d54cae95a8ac3837b5a9c1909fed48666f4
Muhammad Fiqi Firmansyah <fiqifirmansyah5535@gmail.com>
2025-11-23 13:56:54 +0700

    updating hero section v10

--- pages/index.tsx
+++ pages/index.tsx
@@ -204,7 +204,7 @@ const HomePage = () => {
       {/* Stats Section - Moved from Hero */}
       <section className="relative py-12 md:py-16 bg-white dark:bg-gray-800 -mt-16 md:-mt-20 lg:-mt-24">
         {/* Right Trapezoid Shape Extending to Hero - Attached to Stats Rectangle */}
-        <div className="absolute -top-16 md:-top-20 lg:-top-24 left-0 right-0 h-16 md:h-20 lg:h-24 pointer-events-none">
+        <div className="absolute -top-16 md:-top-20 lg:-top-24 left-0 w-1/2 h-16 md:h-20 lg:h-24 pointer-events-none">
           <div 
             className="w-full h-full bg-white dark:bg-gray-800"
             style={{
